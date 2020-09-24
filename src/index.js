eea0f84ae2cb97b2f4292864dab382d08759d244
Terry Alberto Vargas Santa Cruz <terry.vargas@unas.edu.pe>
2020-09-24 02:26:22 -0500

    make it better

--- src/index.js
+++ src/index.js
@@ -27,9 +27,12 @@ app.use('/api/transaction', transactionRouter);
 
 
 
-app.listen(port, host, () => {
-    console.log(`Server run in port ${port}`);
-
+app.set('port', (process.env.PORT || 5000));
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
 });
 
 
